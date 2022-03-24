2adc5afb0a7fcd39ad98ab2bd9aff5a27d87dba6
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2022-03-24 03:38:55 -0400

    added fonts, new theming structure, common components

--- theme/Layout.ts
+++ theme/Layout.ts
@@ -3,10 +3,11 @@ import { Dimensions } from 'react-native';
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 
+const window = { width, height }
+
+const isSmallDevice = width < 375;
+
 export default {
-  window: {
-    width,
-    height,
-  },
-  isSmallDevice: width < 375,
+  window,
+  isSmallDevice
 };
