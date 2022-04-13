8eda817be901a79b1f3237acd93bf5e0e8dfc7c3
Max Pinheiro <maxpinheiro181@gmail.com>
2022-04-12 20:45:36 -0400

    i can spell :D

--- store/App.hooks.ts
+++ store/App.hooks.ts
@@ -2,5 +2,5 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "./App.store";
 
 // create types versions of useDispatch and useSelector
-export const usseAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
